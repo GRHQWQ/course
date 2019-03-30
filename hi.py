9cd4d3dec7c3398763f890c8226a0921c7bd3c99
Henry <ffof@gmail.com>
2019-03-30 17:06:18 +0800

    笔记

---
+++ hi.py
@@ -0,0 +1,65 @@
+'''排序'''
+'''字典 dict 排序'''
+# c = {} #dictionary
+# c[1] = 3
+# c[2] = 'sdfsdfsdfsd'
+# c[5] = 534231
+# c[4] = 'asdasd'
+# # 字典本身没有顺序
+# # list有顺序
+# # key - value
+# # print(c)
+# # for i in c.keys():
+# #     print(i,c[i])
+# keyrecord = c.keys() #list
+# newdiction = sorted(keyrecord) #list
+# print(newdiction)
+# for i in newdiction:
+#     print(i,c[i])
+
+'''list排序'''
+#list
+# a = [1,2,3,4,2,5]
+# a.sort(reverse=True)
+# print(a)
+# a.sort()
+# b = sorted(a)
+# print(a)
+# print(b) 
+
+'''两种遍历方式'''
+a = [5,4,2,6,8]
+
+#第一种 按值来遍历
+# for i in a:
+#     print(i,end='')
+
+#第二种 按序号遍历
+# for i in range(0,len(a)):
+#     print(a[i])
+
+'''二维数组的遍历'''
+
+'''函数的默认参数'''
+#定义函数
+def print_a(a="8888"):
+    print(a)
+
+#调用函数
+print_a("6666")
+
+
+'''git'''
+# 1. 添加 git add .
+# 2. 提交 git commit -m "注释"
+
+# 3. 推送 git push
+
+'''github'''
+# https
+# ssh
+
+'''README'''
+# markdown .md
+# 格式
+
